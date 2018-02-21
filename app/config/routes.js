b745f2beee1bc243e58bc2d918a4bcc94581b4ef
Munna Gupta <munna.g@peopleinteractive.in>
2018-02-21 18:03:09 +0530

    intermediate commit

--- app/config/routes.js
+++ app/config/routes.js
@@ -5,28 +5,23 @@ import { Icon } from 'native-base';
 
 import Home from '../screens/Home';
 
-const HomeStack = StackNavigator(
-	{
-		Home: {
-			screen: Home,
-			navigationOptions: {
-				headerLeft: <Icon name="ios-camera-outline" style={{ paddingLeft: 15 }} />,
-				title: 'Instagram',
-				headerRight: <Icon name="ios-send-outline" style={{ paddingRight: 15 }} />,
-			},
-		},
-	},
-	{}
-);
+const HomeStack = StackNavigator({
+    Home: {
+        screen: Home,
+        navigationOptions: {
+            headerLeft: < Icon name = "ios-camera-outline"
+            style = {
+                { paddingLeft: 15 }
+            }
+            />,
+            title: 'Instagram',
+            headerRight: < Icon name = "ios-send-outline"
+            style = {
+                { paddingRight: 15 }
+            }
+            />,
+        },
+    },
+}, {});
 
-export default StackNavigator(
-	{
-		Home: {
-			screen: HomeStack,
-		},
-	},
-	{
-		mode: 'modal',
-		headerMode: 'none',
-	}
-);
+export default HomeStack;
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
 import { View, StatusBar, Text } from 'react-native';
-import { Container } from '../components/Container';
-import { TabNavigator } from 'react-navigation';
-import { HomeTab } from '../components/TabNavigation/HomeTab';
 
-class Home extends Component {
-	render() {
-		return (
-			<Container>
-				<StatusBar translucent={false} />
-				<View>
-					<Text>hellow world</Text>
-				</View>
-			</Container>
-		);
-	}
+import { Container } from '../components/Container';
+
+export default class Home extends React.Component{
+    render() {
+        return ( 
+        <Container >
+            <StatusBar translucent = { false }/> 
+            <View>
+            <Text> hellow world </Text> 
+            </View > 
+            </Container>
+        );
+    }
 }
